75f1d3b3263671610c2aa50ba839ed0ed11fc6e4
Rudi Arta R <admin@192.168.2.152>
2020-04-01 20:36:42 +0700

    add algo 4

---
+++ algorithm_4/go.mod
@@ -0,0 +1,3 @@
+module github.com/rudiarta/kulina_test/algorithm_4
+
+go 1.14
---
+++ algorithm_4/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	var step int = 1
+	var lamp [100]bool
+	for i := 0; i < 100; i++ {
+		stepAction(step, &lamp)
+		step++
+		if step > 3 {
+			step = 1
+		}
+	}
+	fmt.Println(lamp)
+}
+
+func stepAction(step int, l *[100]bool) {
+	if step == 1 {
+		for i, _ := range l {
+			l[i] = true
+		}
+	} else if step == 2 {
+		for i, _ := range l {
+			if ((i + 1) % 2) == 0 {
+				l[i] = true
+			} else {
+				l[i] = false
+			}
+		}
+	} else if step == 3 {
+		for i, _ := range l {
+			if ((i + 1) % 3) == 0 {
+				l[i] = true
+			} else {
+				l[i] = false
+			}
+		}
+	}
+}
