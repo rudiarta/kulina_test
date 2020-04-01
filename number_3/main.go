44a22c8496d9498f865844953a04a4ab1b4305e1
Rudi Arta R <admin@192.168.2.152>
2020-04-01 17:21:45 +0700

    number 3

--- number_3/main.go
+++ number_3/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 )
 
 func main() {
-	var input int = 1345679
-	data := input % 10000
-	data = (input - data) - 1000000
-	fmt.Println(data)
+	//1345679
+	fmt.Print("input nilai: ")
+	var i int
+	fmt.Scanf("%d", &i)
+	extractInt(i)
+}
+
+func extractInt(n int) {
+	slice := strconv.Itoa(n)
+	var i int = len(slice) - 1
+	for _, v := range slice {
+		compare := math.Pow(10, float64(i))
+		value := int(v-'0') * int(compare)
+		fmt.Println(value)
+		i--
+	}
 }
