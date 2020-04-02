a531e496dcfdc22332e8ce099d182ca7e314e295
rudiarta <rudiarta321@gmail.com>
2020-04-02 07:09:55 +0700

    modify code

--- algorithm_2/main.go
+++ algorithm_2/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	var step int = 0
+	var step int = 1
 	fmt.Print("input banyak data: ")
 	var i int
 	fmt.Scanf("%d", &i)
@@ -16,8 +16,8 @@ func main() {
 func checkStep(s int, step *int) {
 	var x string
 	for d := 0; d < s; d++ {
-		fmt.Println("input step U/D: ")
-		fmt.Scanf("%d", &x)
+		fmt.Printf("input step U/D: ")
+		fmt.Scanf("%s", &x)
 		if x == "U" {
 			*step++
 		}
